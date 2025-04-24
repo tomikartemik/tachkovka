84062b50e83c48ab86f42c609998760e1e2d83cb
tomikartemik <artemules@mail.ru>
2025-04-24 03:54:02 +0300

    tables

--- internal/repository/tables.go
+++ internal/repository/tables.go
@@ -2,6 +2,8 @@ package repository
 
 import (
 	"gorm.io/gorm"
+	"reflect"
+	"strings"
 	"tachkovka/internal/model"
 )
 
@@ -27,22 +29,47 @@ func (r *TablesRepository) GetVersions() ([]model.Version, error) {
 
 func (r *TablesRepository) GetTables(name string) ([]model.Table, error) {
 	tables := []model.Table{}
-	switch name {
-	case "excavator":
-		err := r.db.Model(&model.Excavator{}).Find(&tables).Error
-		return tables, err
-	case "dump_truck":
-		err := r.db.Model(&model.DumpTruck{}).Find(&tables).Error
-		return tables, err
-	case "down_time":
-		err := r.db.Model(&model.DownTime{}).Find(&tables).Error
-		return tables, err
-	case "type_of_work":
-		err := r.db.Model(&model.TypeOfWork{}).Find(&tables).Error
-		return tables, err
-	case "work_place":
-		err := r.db.Model(&model.WorkPlace{}).Find(&tables).Error
-		return tables, err
+
+	// Создаем карту соответствий имен таблиц моделям
+	modelMap := map[string]interface{}{
+		"version":      &model.Version{},
+		"excavator":    &model.Excavator{},
+		"dump_truck":   &model.DumpTruck{},
+		"down_time":    &model.DownTime{},
+		"type_of_work": &model.TypeOfWork{},
+		"work_place":   &model.WorkPlace{},
 	}
+
+	// Приводим имя к нижнему регистру для унификации
+	name = strings.ToLower(name)
+
+	// Получаем модель из карты
+	modelInstance, ok := modelMap[name]
+	if !ok {
+		return tables, nil
+	}
+
+	// Используем рефлексию для создания слайса нужного типа
+	sliceType := reflect.SliceOf(reflect.TypeOf(modelInstance))
+	slice := reflect.New(sliceType).Interface()
+
+	// Выполняем запрос
+	err := r.db.Model(modelInstance).Find(slice).Error
+	if err != nil {
+		return nil, err
+	}
+
+	// Конвертируем результат в []model.Table
+	result := reflect.ValueOf(slice).Elem()
+	for i := 0; i < result.Len(); i++ {
+		item := result.Index(i).Interface()
+		id := reflect.ValueOf(item).FieldByName("ID").Int()
+		name := reflect.ValueOf(item).FieldByName("Name").String()
+		tables = append(tables, model.Table{
+			ID:   int(id),
+			Name: name,
+		})
+	}
+
 	return tables, nil
 }
